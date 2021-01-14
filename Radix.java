894be56e5fe5f614d9b8532e06aee4e59c11ad08
thomasyu21 <>
2021-01-14 12:02:15 -0500

    Fixed radixSimpleSort to make runtime O(n) rather than O(n^2). Also made some changes to nth and length to make the methods faster.

--- Radix.java
+++ Radix.java
@@ -1,19 +1,14 @@
 public class Radix{
   public static int nth(int n, int col){
-    int temp = n / (int)Math.pow(10, col);
-    return Math.abs(temp%10);
+    return Math.abs((n / (int)Math.pow(10, col))%10);
   }
+
 
   public static int length(int n){
     if (n == 0){
       return 1;
     }
-    int count = 0;
-    while (n != 0){
-      n = n / 10;
-      count++;
-    }
-    return count;
+    return (int)Math.log10(Math.abs(n)) + 1;
   }
 
   public static void merge(SortableLinkedList original, SortableLinkedList[]buckets){
@@ -23,10 +18,7 @@ public class Radix{
   }
 
   public static void radixSortSimple(SortableLinkedList data){
-    int mostDigits = 0;
-    for (int i = 0; i < data.size(); i++){
-       mostDigits = Math.max(length(data.get(i)), mostDigits);
-    }
+    int mostDigits = 1;
     SortableLinkedList[]buckets = new SortableLinkedList[10];
     for (int i = 0; i < buckets.length; i++){
       buckets[i] = new SortableLinkedList();
@@ -35,6 +27,9 @@ public class Radix{
     for (int i = 0; i < mostDigits; i++){
       for (int j = 0; j < size; j++){
         int temp = data.remove(0);
+        if (i == 0){
+          mostDigits = Math.max(length(temp), mostDigits);
+        }
         buckets[nth(temp, i)].add(temp);
       }
       merge(data, buckets);
